c2d0893e91c5fa1475739d59fbd928389a1bd6cd
Nekrasov Dmitriy <dimannekr@mail.ru>
2023-01-20 07:12:27 +0700

    Add check of meeting date

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -16,15 +16,17 @@ public class CardDeliveryTest {
     public void shouldCorrectWorkTest() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Новосибирск");
-        String a = $ ("[data-test-id=date] input").getValue();
+        String planningDate = $("[data-test-id=date] input").getValue();
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(a);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//*[@data-test-id='name']//input").setValue("Петров Василий");
         $x("//*[@data-test-id='phone']//input").setValue("+79184582645");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
-        $x("//div[@data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 }
